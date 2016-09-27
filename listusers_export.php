3923fdb8e2b53d9b06ac9e0873fe2fdbea1065e6
Tii <tii@stevie-ray.me>
2016-09-27 20:01:25 +0200

    Verwenden der ominösen E-Funktion

--- listusers_export.php
+++ listusers_export.php
@@ -16,19 +16,23 @@ function vcard_printf(/* $format, ...$args */) {
 	echo vcard_print($line);
 }
 
+function vcard_E($str) {
+	return html_entity_decode(E($str));
+}
+
 /**
  * Prints a complete VCARD for the given userobject.
  * $user The user object, that should be converted to VCARD
  */
 function vcard_print_person($user) {
 	// Get data from user object
-	$nameparts = explode(" ", $user["displayname"]);
+	$nameparts = explode(" ", vcard_E($user["displayname"]));
 
 	$firstname = implode(" ", array_slice($nameparts, 0, -1));
 	$lastname = end($nameparts);
-	$mobile = $user["mobile"];
-	$homephone = $user["homephone"];
-	$email = $user["uid"] ."@d120.de";
+	$mobile = vcard_E($user["mobile"]);
+	$homephone = vcard_E($user["homephone"]);
+	$email = vcard_E($user["uid"]) ."@d120.de";
 
 	// Print vcard for this user
 	vcard_print("BEGIN:VCARD");
