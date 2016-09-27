76b3b77332fe977b4bc53b2685bbc2d3d38feb78
Tii <tii@stevie-ray.me>
2016-09-27 20:47:10 +0200

    Diverse Änderungen und Anpassungen

--- listusers_export.php
+++ listusers_export.php
@@ -26,24 +26,30 @@ function vcard_E($str) {
  */
 function vcard_print_person($user) {
 	// Get data from user object
-	$nameparts = explode(" ", vcard_E($user["displayname"]));
-
-	$firstname = implode(" ", array_slice($nameparts, 0, -1));
-	$lastname = end($nameparts);
+	// MUST exist
+	$firstname = vcard_E($user["givenName"]);
+	$lastname = vcard_E($user["sn"]);
+	// MAY exist
 	$mobile = vcard_E($user["mobile"]);
 	$homephone = vcard_E($user["homephone"]);
 	$email = vcard_E($user["mail"]);
+	$birthyear = vcard_E($user["birthyear"]);
+	$birthmonth = vcard_E($user["birthmonth"]);
+	$birthday = vcard_E($user["birthday"]);
 
 	// Print vcard for this user
 	vcard_print("BEGIN:VCARD");
 	vcard_print("VERSION:3.0");
 	vcard_printf("FN:%s %s", $firstname, $lastname);
 	vcard_printf("N:%s;%s;;;", $lastname, $firstname);
-	vcard_printf("EMAIL;TYPE=Fachschaft:%s", $email);
+	if ($email)
+		vcard_printf("EMAIL;TYPE=Fachschaft:%s", $email);
 	if ($mobile)
 		vcard_printf("TEL;TYPE=cell:%s", $mobile);
 	if ($homephone)
 		vcard_printf("TEL;TYPE=home:%s", $homephone);
+	if ($birthyear && $birthmonth && $birthday)
+		vcard_printf("BDAY:%'02u-%'02u-%'02u", $birthyear, $birthmonth, $birthday);
 	vcard_print("END:VCARD");
 }
 
